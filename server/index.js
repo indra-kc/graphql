0af25c3a434c9757e367d318ff3261dbb7a150c0
indra-kc <159883430+indra-kc@users.noreply.github.com>
2024-11-01 06:07:02 +0000

    feat: backend graphql

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+async function startServer() {
+    const app = express();
+    const server = new ApolloServer({
+        typeDefs: `
+        type Todo{
+        id:ID!
+        title:String!
+        completed:Boolean
+        }
+        
+        type Query{
+        getTodos: [Todo]
+        }
+        `,
+        resolvers: {
+            Query:{
+                getTodos:()=>[{id:1, title:'something', completed:false}]
+            }
+        },
+    });
+
+    app.use(bodyParser.json());
+    app.use(cors());
+
+    await (server.start());
+
+    app.use('/graphql', expressMiddleware(server));
+
+    app.listen(8000, () => console.log("server started at PORT 8000"));
+}
+
+startServer();
